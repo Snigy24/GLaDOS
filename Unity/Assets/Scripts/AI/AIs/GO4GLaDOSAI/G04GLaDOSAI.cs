a72c7a704826c843e69bbbfe13b1411d27284555
Insoel <insoel@gmail.com>
2020-01-03 18:32:47 +0000

    Create AI classes

---
+++ Unity/Assets/Scripts/AI/AIs/GO4GLaDOSAI/G04GLaDOSAI.cs
@@ -0,0 +1,20 @@
+﻿
+public class G04GLaDOSAI : AIPlayer
+{
+	public override string PlayerName => "GLaDOSAI";
+
+	public override IThinker Thinker => thinker;
+
+	// Suport variable for RandomAI's thinker instance
+	private IThinker thinker;
+
+	/// <summary>
+	/// This method will be called before a match starts and is used for
+	/// instantiating a new <see cref="RandomAIThinker"/>.
+	/// </summary>
+	/// <seealso cref="AIPlayer.Setup"/>
+	public override void Setup()
+	{
+		thinker = new RandomAIThinker();
+	}
+}
---
+++ Unity/Assets/Scripts/AI/AIs/GO4GLaDOSAI/G04GLaDOSAIThinker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+public class G04GLaDOSAIThinker : IThinker
+{
+	// A random number generator instance
+	private Random random;
+
+
+	/// <summary>
+	/// Create a new instance of G04GLaDOSAIThinker.
+	/// </summary>
+	public G04GLaDOSAIThinker()
+	{
+		random = new Random();
+	}
+
+	public FutureMove Think(Board board, int depth, int alpha, int beta, bool maximizingPlayer, CancellationToken ct)
+	{
+		if (depth == 0 || board.CheckWinner() != Winner.None)
+			//return
+
+		if (maximizingPlayer)
+			{
+				board.
+				int maxEval = int.MinValue;
+				
+			}
+	}
+}
